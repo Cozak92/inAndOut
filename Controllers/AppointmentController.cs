167632e676fd9b7d9ba88a2c6be65b22128eac82
Cozak92 <Cozak92@gmail.com>
2021-04-18 17:12:19 +0900

    2021/04/18

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Details(string name)
+        {
+            return Ok($"You have enterd name: { name}");
+
+        }
+    }
+}
