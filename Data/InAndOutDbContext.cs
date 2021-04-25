5d008b315e0dca92cb7f76d8b36c0c2e486da140
Cozak92 <Cozak92@gmail.com>
2021-04-25 10:57:02 +0900

    2021/04/25

---
+++ Data/InAndOutDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InAndOut.Data
+{
+    public class InAndOutDbContext :DbContext
+    {
+        public InAndOutDbContext(DbContextOptions<InAndOutDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace InAndOut.Models
+{
+    public class Item
+    {
+        [Key] //ASP.NET에서는 ID를 자동적으로 프라이머리 키로 설정하므로 실제로는 작성하지 않아도 된다.
+        public int Id { get; set; }
+
+        public string Borrower { get; set; }
+
+
+    }
+}
